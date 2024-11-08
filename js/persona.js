bc155f66f22d6c91bf2f09b79df78beac8ab28d2
Bruno Nicolás Villoria Maulin <b.n.v.m.94@gmail.com>
2024-11-07 23:13:15 -0300

    small changes

---
+++ js/persona.js
@@ -0,0 +1,57 @@
+const persona = {
+    nombre: "Roy Richardson",
+    mail: "Mail: roy.richardson@example.com",
+    nacimiento: "Nacimiento: 3-4-1945",
+    dir: "Dir: 7001 James St",
+    tel: "Tel: (234) 454-5436",
+    pres: "Soy Roy, oriundo de New Zeland, practico basquet y surfing, me dedico a programar en mis ratos libres.........",
+    hab: "C++, Web-design, JavaScript"
+};
+
+// elemento a crear, formato string, el array en formato string y id, en formato string
+function poner_elemento_y_texto_desde_array(elemento_a_crear, array, id) {
+    var elemento = document.createElement(elemento_a_crear);
+    var texto = document.createTextNode(array);
+    elemento.appendChild(texto);
+    var direccion = document.getElementById(id);
+    direccion.appendChild(elemento);
+}
+
+function toggle_mostrar(showId, ...hideIds) {
+    document.getElementById(showId).style.display = "initial";
+    hideIds.forEach(id => document.getElementById(id).style.display = "none");
+}
+
+function poner_texto_a_elemento(id, texto) {
+    document.getElementById(id).innerHTML = texto;
+}
+
+poner_elemento_y_texto_desde_array("h1", persona.nombre, "titulo");
+
+
+document.getElementById('pres_bot').addEventListener('click', function () {
+    toggle_mostrar("p1", "ul1", "p2");
+    // document.getElementById('ul1').style.display = "none";
+    // document.getElementById('p2').style.display = "none";
+    // document.getElementById('p1').style.display = "initial";
+    poner_texto_a_elemento("p1", persona.pres);
+})
+
+document.getElementById('hab_bot').addEventListener('click', function () {
+    toggle_mostrar("p2", "ul1", "p1");
+    // document.getElementById('ul1').style.display = "none";
+    // document.getElementById('p2').style.display = "initial";
+    // document.getElementById('p1').style.display = "none";
+    poner_texto_a_elemento("p2", persona.hab);
+})
+document.getElementById('cont_bot').addEventListener('click', function () {
+    toggle_mostrar("ul1", "p1", "p2");
+    // document.getElementById('p1').style.display = "none";
+    // document.getElementById('ul1').style.display = "initial";
+    // document.getElementById('p2').style.display = "none";
+    poner_texto_a_elemento("li1", persona.mail);
+    poner_texto_a_elemento("li2", persona.nacimiento);
+    poner_texto_a_elemento("li3", persona.dir);
+    poner_texto_a_elemento("li4", persona.tel);
+
+})
